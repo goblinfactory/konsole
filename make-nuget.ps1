02e7d9e7d3b30adc020708e1f88a97390de2a14d
Alan Hemmings <goblinfactory@gmail.com>
2017-03-18 17:34:05 +0000

    update nuget build

--- make-nuget.ps1
+++ make-nuget.ps1
@@ -2,5 +2,5 @@
 $dir= '.\Konsole\'
 Push-Location
 cd $dir
-nuget pack .\Konsole.csproj -Prop Configuration=Release
+..\packages\NuGet.CommandLine.3.5.0\tools\nuget pack .\Konsole.csproj -Prop Configuration=Release
 Pop-Location
