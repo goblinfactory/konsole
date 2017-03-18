ea7b486767e10d11e507bebc7905de25acb62654
Alan Hemmings <goblinfactory@gmail.com>
2017-03-18 18:37:19 +0000

    small tweak build scripts

--- make-nuget.ps1
+++ make-nuget.ps1
@@ -2,5 +2,5 @@
 $dir= '.\Konsole\'
 Push-Location
 cd $dir
-..\packages\NuGet.CommandLine.3.5.0\tools\nuget pack .\Konsole.csproj -Prop Configuration=Release
+nuget pack .\Konsole.csproj -Prop Configuration=Release
 Pop-Location
--- push-nuget.ps1
+++ push-nuget.ps1
@@ -1,2 +1,2 @@
 # TODO - write script to ask which version or work it out from latest modified?
-nuget push Konsole\Goblinfactory.Konsole.1.0.0.0.nupkg
+nuget push Konsole\Goblinfactory.Konsole.2.0.0.nupkg -Source https://www.nuget.org/api/v2/package
