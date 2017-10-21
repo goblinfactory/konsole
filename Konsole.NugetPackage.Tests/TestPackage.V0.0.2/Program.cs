6cffdf06b238d7514af2e6f092dfa7c519ee52a7
Alan Hemmings <goblinfactory@gmail.com>
2017-10-21 19:31:41 +0100

    Remove package test projects from core code, declutter.

---
+++ Konsole.NugetPackage.Tests/TestPackage.V0.0.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Konsole;
+
+namespace TestPackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new ProgressBar(50);
+            pb.Refresh(0, "connecting to server to download 50 files sychronously.");
+            Console.ReadLine();
+            Console.Write("This");
+            pb.Refresh(25, "downloading file number 25");
+            Console.Write(" should all");
+            Console.ReadLine();
+            pb.Refresh(50, "finished.");
+            Console.WriteLine(" be on the same line.");
+
+        }
+    }
+}
