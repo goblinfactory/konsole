6cb3bf5f5ed82fd74f59c6d4c671759907211d05
Alan Hemmings <goblinfactory@gmail.com>
2019-10-08 19:26:54 +0100

    update project to new project type. Plus test project for package 3.4.0.

---
+++ Konsole.NugetPackage.Tests/TestPackage.V3.4.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Konsole;
+using Konsole.Layouts;
+
+namespace TestPackage.V3._4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new ProgressBar(10);
+
+            for (int i =0; i<11; i++)
+            {
+                pb.Refresh(i, "cat "+ i);
+                Console.ReadKey(true);
+            }
+            
+            Console.ReadKey(true);
+            var w = new Window();
+            var left = w.SplitLeft("left");
+            w.SplitRight("right");
+            var top = left.SplitTop("top");
+            var bottom = left.SplitBottom("bottom");
+
+            for (int i = 0; i < 10; i++)
+            {
+                top.WriteLine(i.ToString());
+                bottom.WriteLine((100-i).ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
