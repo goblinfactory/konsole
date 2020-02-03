ac82d725a30e0700af9a796d94ef9e11b5ec373c
Alan Hemmings <goblinfactory@gmail.com>
2020-02-03 21:58:41 +0000

    small spike proof of concept, highlighting active item.

--- src/Konsole.Samples/Program.cs
+++ src/Konsole.Samples/Program.cs
@@ -8,31 +8,84 @@ namespace Konsole.Samples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello there");
-            Console.ReadKey(true);
+            Console.CursorVisible = false;
             int y = Console.CursorTop;
-            while(true)
+            Console.ForegroundColor = White;
+            Console.BackgroundColor = DarkBlue;
+
+            var normal = new BoxStyle(
+                LineThickNess.Single,
+                body: new Colors(Gray, DarkBlue),
+                line: new Colors(Gray, Black),
+                title: new Colors(Gray, Black)
+            );
+
+            var active = new BoxStyle(
+                LineThickNess.Single,
+                body: new Colors(White, DarkBlue),
+                line: new Colors(White, DarkBlue),
+                title: new Colors(White, DarkBlue)
+            );
+
+            while (true)
             {
-                Render(y + 2, Blue, White, Yellow, new Colors(White, Red));
+                RenderGames(y + 2, active);
+                RenderUsers(y + 2, normal);
                 Console.ReadKey(true);
-                Render(y + 2, DarkBlue, White, Yellow, new Colors(White, Red));
+                
+                RenderGames(y + 2, normal);
+                RenderUsers(y + 2, active);
                 Console.ReadKey(true);
             }
-            //Console.CursorVisible = false;
+            
             //ListViewSampleFileBrowser.Main(null);
 
         }
 
-        static void Render(int sy, ConsoleColor bg, ConsoleColor fg, ConsoleColor line, Colors title)
+        static void RenderUsers(int sy, BoxStyle style)
         {
-            var window = Window.OpenBox("users", 5, sy,  25, 5, new BoxStyle { Body = new Colors(fg, bg), Line = new Colors(line, bg), Title = title });
-            var view = new ListView<(string Name, int Credits)>(
+            var window = Window.OpenBox("players", 7, sy,  35, 7, style);
+            var view = new ListView<(string Name, int Credits, string IPAddress)>(
                 window,
-                () => new[] { ("Fred", 250), ("Sally", 100) }, (u) => new[] { u.Name, u.Credits.ToString("00000") },
+                () => new[] { 
+                    ("Fred", 250, "80.56.12.11"), 
+                    ("Sally", 100, "74.11.23.44"), 
+                    ("Michael", 0, "63.11.11.40"), 
+                    ("Dennis", 0, "88.22.23.12")  }, (u) => new[] { 
+                        u.Name, 
+                        u.Credits.ToString("00000"),
+                        u.IPAddress
+                    },
                 new Column("Name", 0),
-                new Column("Credits", 0)
+                new Column("Credits", 0),
+                new Column("IP Address", 0)
             );
             view.Refresh();
         }
+
+        static void RenderGames(int sy, BoxStyle style)
+        {
+            var window = Window.OpenBox("openings", 50, sy, 35, 12, style);
+            var view = new ListView<(string opening, int moves, string result)>(
+                window,
+                () => new[] {
+                    ("Kings Gambit", 39, "win"),
+                    ("Sicilian Defence", 35, "draw"),
+                    ("French Defence", 22, "win"),
+                    ("Alekhine Defence", 19, "win"),
+                    ("Kings Gambit", 33, "win"),
+                    ("Kings Indian", 21, "draw"),
+                    ("Ruy Lopaz", 82, "lose")  }, (u) => new[] {
+                        u.opening,
+                        u.moves.ToString(),
+                        u.result,
+                    },
+                new Column("Opening", 0),
+                new Column("Moves", 7),
+                new Column("Result", 7)
+            );
+            view.Refresh();
+        }
+
     }
 }
