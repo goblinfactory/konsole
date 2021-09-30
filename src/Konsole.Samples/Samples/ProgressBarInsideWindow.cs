7413d5683749be4f3300e4881cb895845e51c53b
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2021-09-30 14:18:43 +0200

    Update ProgressBarInsideWindow.cs
    matching the readme and adding Console.ReadLine(); to keep the console open.
    This will benefit beginners.

--- src/Konsole.Samples/Samples/ProgressBarInsideWindow.cs
+++ src/Konsole.Samples/Samples/ProgressBarInsideWindow.cs
@@ -10,14 +10,16 @@ namespace Konsole.Samples
         public static void Main(string[] args)
         {
             var w = Window.OpenBox("tasks", 60, 8);
-            var right = w.SplitLeft("files");
-            var left = w.SplitRight("users");
-            
-            var pb1 = new ProgressBar(left, 100);
-            pb1.Refresh(50, "hotel-california.mp3");
+            var left = w.SplitLeft("files");
+            var right = w.SplitRight("users");
 
-            var pb2 = new ProgressBar(right, 100);
-            pb2.Refresh(10, "Clint Eastwood");
+            var pb1 = new ProgressBar(right, 100);
+            pb1.Refresh(10, "Clint Eastwood");
+
+            var pb2 = new ProgressBar(left, 100);
+            pb2.Refresh(50, "hotel-california.mp3");
+
+            Console.ReadLine();
         }
         //end-snippet
     }
